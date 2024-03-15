d0116102461fca953b9dc0f6aaceca20064fccef
Bob Lee <bob@twingate.com>
2024-03-15 15:29:52 -0400

    change pypi name for testing

--- provider/resources.go
+++ provider/resources.go
@@ -143,6 +143,7 @@ func Provider() tfbridge.ProviderInfo {
 			//Overlay: &tfbridge.OverlayInfo{},
 		},
 		Python: &tfbridge.PythonInfo{
+			PackageName: "emailbob_twingate",
 			// List any Python dependencies and their version ranges
 			Requires: map[string]string{
 				"pulumi": ">=3.0.0,<4.0.0",
